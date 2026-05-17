ceecc0ad6b2438b9b8151926058e231fe7301be4
ZHOU DA <zd205428@outlook.com>
2026-05-17 21:55:38 +0800

    feat(python):实现下载器，只下载课件相关资源

--- python-service/app/course_resource_importer/__init__.py
+++ python-service/app/course_resource_importer/__init__.py
@@ -1,5 +1,6 @@
 from .authorized_fetcher import fetch_authorized_html, sanitize_headers_for_log
 from .chaoxing_ref import build_chaoxing_course_url, parse_chaoxing_course_url
+from .downloader import DownloadPlan, DownloadResult, build_download_plan, download_plan
 from .html_resource_discoverer import discover_resources_from_html
 from .js_resource_discoverer import discover_resources_from_js_text
 from .json_resource_discoverer import discover_resources_from_json
@@ -19,6 +20,8 @@ __all__ = [
     "AuthorizedFetchContext",
     "ChaoxingCourseRef",
     "CourseResourceImporterError",
+    "DownloadPlan",
+    "DownloadResult",
     "DownstreamFetchError",
     "DiscoveredResource",
     "ImageProbe",
@@ -27,11 +30,13 @@ __all__ = [
     "UnauthorizedFetchError",
     "UnsafeUrlError",
     "build_chaoxing_course_url",
+    "build_download_plan",
     "classify_resource",
     "classify_resources",
     "discover_resources_from_html",
     "discover_resources_from_js_text",
     "discover_resources_from_json",
+    "download_plan",
     "fetch_authorized_html",
     "parse_chaoxing_course_url",
     "sanitize_headers_for_log",
